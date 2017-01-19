02bbd2f8fb17ff1e0289e4e39036fee917246256
Rod Widdowson <rdw@steadingsoftware.com>
2017-01-19 14:13:08 +0000

    EDS-78 rescale on height > maxHeight doesn't work
    https://issues.shibboleth.net/jira/browse/EDS-78
    
    Reviewed and applied supplied patch.

--- src/javascript/idpselect.js
+++ src/javascript/idpselect.js
@@ -632,7 +632,7 @@ function IdPSelectUI() {
         }
         if (h> maxHeight) {
             w = (maxHeight/h) * w;
-            w = maxHeight;
+            h = maxHeight;
         }
             
         img.setAttribute('width', w);
